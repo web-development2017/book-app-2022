8c1e3f35180516dac89814a84a84ece3ec37da80
Rich <web_dev2017@outlook.com>
2022-08-12 17:47:18 +0100

    updated link to icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,13 +18,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
